ce15f8a362ad77c810077590c6f40e335408c98e
aayushmalla13 <32125193+aayushmalla13@users.noreply.github.com>
2021-04-01 12:32:20 +0545

    Add files via upload

--- finalquerynew.sql
+++ finalquerynew.sql
@@ -1,15 +1,3 @@
--------FINAL QUERY----------
-select vw_bot20.cName,vw_bot20.totalCases, vw_top20.cName,vw_top20.totalCases,totalNum.case_type,totalNum.case_count,vw_newdata.cName,vw_newdata.newCases,vw_newdata.newDeaths
-from vw_bot20   join vw_top20
-on vw_bot20.ids = vw_top20.ids 
- left join totalNum
-on
-vw_top20.ids= totalNum.ids
-left join vw_newdata
-on
-vw_top20.ids=vw_newdata.ids;
-
-
 
             with remres as (
             SELECT 2 ids,'TotalCases:' E ,' ' F, ' ' G,' ' H,  'Deaths:' d ,' ' M, 'Recovered: ' N, ' ' O from dual
@@ -44,21 +32,4 @@ vw_top20.ids=vw_newdata.ids;
             union
             SELECT 16 ids,' ' E , nepal, ' ' G,' ' H,  ' ' d ,' ' M, ' ' N, ' ' O from vw_newdata
     
-            )select vw_bot20.cName "TOP" ,vw_bot20.totalCases "20",remres.E,remres.F,remres.G,remres.H,remres.d,remres.M,remres.N,remres.O, vw_top20.cName "BOTTOM" ,vw_top20.totalCases " 20"
-from vw_bot20    left join remres
-on vw_bot20.ids=remres.ids
- left join vw_top20
-on vw_bot20.ids = vw_top20.ids ;
-   
-
-
---
-
---CREATE OR REPLACE VIEW vw_newdata as
---select  'NEPAL Position:'|| ''||rankFinder('NEPAL') country,'Total:'||ci.totalcases ||' New:'|| nc.newCases cases,'Total Deaths:'||ci.totaldeaths ||' New Deaths:'|| nc.newDeaths deaths from newcovidInfo nc
---inner join covidInfo ci
---on
---nc.cName = ci.cName
---where upper(nc.cName)='NEPAL';
-----select * from vw_newdata;
---select 20,country,datas from vw_newdata;
+            ) select * from remres; 
